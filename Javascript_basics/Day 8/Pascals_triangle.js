1efcc39bba21286ad0053c93a8c81ddf350f84f6
Aman Verma <85391221+amanverma2908@users.noreply.github.com>
2022-11-04 22:05:50 +0530

    Update Pascals_triangle.js

--- Javascript_basics/Day 8/Pascals_triangle.js
+++ Javascript_basics/Day 8/Pascals_triangle.js
@@ -16,4 +16,4 @@ var generate = function(numRows) {
 let n = prompt("Enter number of rows");
 //pass number of rows in generate function.
 
-console.log((n>0) ? generate(n) : "Enter a positive number!");
+console.log((n>0) ? generate(n) : "Enter a positive value!");
