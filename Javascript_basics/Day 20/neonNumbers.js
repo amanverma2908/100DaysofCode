ee0c605b234b77335efb6959c739c751ecff9b71
Aman Verma <85391221+amanverma2908@users.noreply.github.com>
2022-11-09 20:22:16 +0530

    Create neonNumbers.js

---
+++ Javascript_basics/Day 20/neonNumbers.js
@@ -0,0 +1,13 @@
+let num = prompt("Enter the number: ");
+let sqr = num * num; 
+let sumOfDigit = 0;
+    
+while (sqr > 0) {
+       sumOfDigit += sqr % 10;
+       sqr = Math.floor(sqr / 10);
+}
+           
+if (num == sumOfDigit)
+    console.log("Neon Number");
+else
+    console.log("Not a Neon Number");
